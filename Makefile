b56cae38b62657bd65bf0019ad83de7362a45cb6
Peter Engelbert <pmengelbert@gmail.com>
2021-04-26 18:46:44 -0500

    Add comments and simplify
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
-.PHONY: clean
+.PHONY: clean push
 
 zblink: zblink.o
-	ld -o zblink zblink.o
+	PATH=/usr/arm-linux-gnueabihf/bin ld -o zblink zblink.o
 
 zblink.o: clean
-	as -o zblink.o blink.s
+	PATH=/usr/arm-linux-gnueabihf/bin as -o zblink.o blink.s
 
 clean:
 	rm zblink zblink.o || true
 
+push: zblink
+	scp zblink pi@10.1.10.10:blink
--- blink.s
+++ blink.s
@@ -1,10 +1,10 @@
 PAGE_SIZE = 0x1000 @ standard linux page size
 
 GPIO_OFFSET = 0x20200 /* 
-						 See page 5 of BCM2835 datasheet, the "physical addresses" section.
-						 It's divided by 0x1000 because mmap2(2) takes the number of pages,
-						 not the number of bytes.
-					  */
+                         See page 5 of BCM2835 datasheet, the "physical addresses" section.
+                         It's divided by 0x1000 because mmap2(2) takes the number of pages,
+                         not the number of bytes.
+                      */
 GPIO_PIN_OUTPUT = 0xfffffe7f @ see note below
 
 .data
@@ -15,7 +15,7 @@ sleep_amount: .4byte 1, 0 @ the amount of time between blinks. See nanosleep(2).
 .globl _start
 
 _start:
-	@ open /dev/gpiomem to pass to mmap2
+    @ open /dev/gpiomem to pass to mmap2
     mov r7, #5
     ldr r0, =filename
     mov r1, #2
@@ -26,27 +26,38 @@ _start:
     mov r4, r0
 
     @ mmap2. See mmap2(2)
+    /*
+        tldr: mmap2 will map a file (including special files like
+        /dev/gpiomem) to the current process's address space. /dev/mem (or
+        /dev/gpiomem) maps to the physical addresses of the machine.
+
+        the return value of mmap2 is a pointer to memory owned by the local
+        process, which if acted upon will affect the source of the map (i.e.
+        /dev/gpiomem).
+    */
     mov r0, #0
     mov r1, #PAGE_SIZE
-    mov r2, #3
-    mov r3, #1 
-    ldr r5, =#GPIO_OFFSET
+    mov r2, #3 @ read and write access
+    mov r3, #1 @ this is shared access
+    ldr r5, =#GPIO_OFFSET @ the physical address of the GPIO pins
     mov r7, #192
     svc #0
 
     @ return value is base address of GPIO peripheral. Store this address in r8
     mov r8, r0
 
-    @ for pin n:
-    @ register is BASE_GPIO + n/10 (integer division)
-    @ formula for function set to output: ~(7 << ((n % 10)*3)) | 1 << ((n % 10)*3)
-    @ set the mode of pin 2 to output
+    /*
+        for pin n:
+        register is BASE_GPIO + n/10 (integer division)
+        formula for function set to output: ~(7 << ((n % 10)*3)) | 1 << ((n % 10)*3)
+        set the mode of pin 2 to output
+    */
     ldr r0, =#GPIO_PIN_OUTPUT
     str r0, [r8]
 
-loop:
+.Lloop:
     @ clear pin 2
-    mov r0, #4
+    mov r0, #4 @ the formula for setting or clearing pin n: 1 << n. Here, 1 << 2 = 4
     str r0, [r8, #0x28]
 
     @ nanosleep(2) syscall
@@ -64,9 +75,11 @@ loop:
     mov r7, #162
     svc #0
 
-    b loop
+    b .Lloop
 
     mov r7, #1
     mov r0, #0
  
     svc #0
+
+@ vim:ft=armv5
