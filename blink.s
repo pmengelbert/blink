b06251ab4ad179b624f1dcabc4b34a138136a953
Peter Engelbert <pmengelbert@gmail.com>
2021-04-26 05:13:11 +0100

    It worksgit add -Agit add -A!
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- blink.s
+++ blink.s
@@ -1,11 +1,15 @@
 GPIO_OFFSET = 0x20200
 PAGE_SIZE = 0x1000
+GPIO_PIN_OUTPUT = 0xfffffe7f
 
 .data
 filename: .asciz "/dev/gpiomem"
+@ sleep_amount: .word #4
+@ .word #4
 
 .bss
 .lcomm gpio_filedes, 4
+.lcomm gpio, 4
 
 .text
 .globl _start
@@ -14,29 +18,41 @@ filename: .asciz "/dev/gpiomem"
 _start:
     mov r7, #5
     ldr r0, =filename
-    ldr r0, [r0]
     mov r1, #2
     mov r2, #0
     svc #0
 
-
     ldr r8, =gpio_filedes
     str r0, [r8]
 
+    @ mmap2
     mov r0, #0
     mov r1, #PAGE_SIZE
     mov r2, #3
     mov r3, #1 
-    mov r4, [r8]
-    mov r5, #GPIO_OFFSET
+    ldr r4, [r8]
+    ldr r5, =#GPIO_OFFSET
     mov r7, #192
     svc #0
 
-    @ close the file
-    @ mov r7, #6
-    @ svc #0
+    @ store GPIO address
+    ldr r8, =gpio
+    str r0, [r8]
+    ldr r8, [r8]
 
-    @ exit
+    @ set the mode of pin 2 to output
+    ldr r0, =#GPIO_PIN_OUTPUT
+    str r0, [r8]
+
+    @ clear pin 2
+    mov r0, #4
+    str r0, [r8, #0x28]
+
+    @ turn on pin 2
+    mov r0, #4
+    str r0, [r8, #0x1c]
+
     mov r7, #1
     mov r0, #0
+ 
     svc #0
