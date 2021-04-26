4bb7275149a60b64ac0971ba39cbca1d6a27c8a0
Peter Engelbert <pmengelbert@gmail.com>
2021-04-26 01:01:39 +0100

    will this work?
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- blink.s
+++ blink.s
@@ -1,4 +1,4 @@
-GPIO_OFFSET = 0x20200000
+GPIO_OFFSET = 0x20200
 PAGE_SIZE = 0x1000
 
 .data
@@ -13,21 +13,30 @@ filename: .asciz "/dev/gpiomem"
 
 _start:
     mov r7, #5
-    ldr r0, =addr_filename
+    ldr r0, =filename
     ldr r0, [r0]
     mov r1, #2
     mov r2, #0
     svc #0
 
-    ldr r8, =gpio_filedes
-    str r0, [r8]
 
-    mov r7, #6
+    ldr r8, =gpio_filedes
+    str r0, [r8] ; store the file descriptor somewhere
+
+    mov r0, #0 ; NULL means address will be determined for me
+    mov r1, #PAGE_SIZE
+    mov r2, #3 ; read and write
+    mov r3, #1 ; shared
+    mov r4, [r8]
+    mov r5, #GPIO_OFFSET
+    mov r7, #192 ; mmap2
     svc #0
 
+    ; close the file
+    ; mov r7, #6
+    ; svc #0
+
+    ; exit
     mov r7, #1
     mov r0, #0
     svc #0
-
-addr_filename: .word filename
-    
